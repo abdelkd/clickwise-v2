1ecdd8e0589dfdc0abf5c254ec2e5029727d667e
abdelkd <abdelkarimdjelalda@gmail.com>
2024-05-28 13:40:02 +0100

    feat: add prettier, husky

---
+++ prettier.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('prettier').Config} */
+const prettierConfig = {
+  singleQuote: true,
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+};
+
+export default prettierConfig;
